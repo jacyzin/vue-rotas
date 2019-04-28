5f93e67f85bf4d77fa42287845af9e7bbfc4125a
kodemann <henryjacyzin@gmail.com>
2019-04-28 19:35:05 -0300

    Add beforeRouteUpdate

--- src/views/contatos/ContatoDetalhes.vue
+++ src/views/contatos/ContatoDetalhes.vue
@@ -3,6 +3,20 @@
 </template>
 <script>
     export default {
+        data() {
+          return {
+              id: this.$route.params.id
+          }
+        },
+      /*  watch: {
+          '$route'(to, from) {
+              this.id = to.params.id
+          }
+        },*/
+      beforeRouteUpdate(to, from , next) {
+          this.id = to.params.id
+          next()
+      },
         created() {
             console.log('Parâmetros da rota:', this.$route.params);
         }
