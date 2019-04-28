ec02c213975a0445ca9f900c663134038a374ccb
kodemann <henryjacyzin@gmail.com>
2019-04-28 18:48:20 -0300

    Add active class for buttons

--- src/main.js
+++ src/main.js
@@ -1,25 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router'
-import Contatos from './views/contatos/Contatos.vue'
-import Home from './views/Home.vue'
+import router from './router'
 
 Vue.config.productionTip = false;
-Vue.use(VueRouter);
-
-const router = new VueRouter({
-  mode: 'history',
-  routes: [
-    {
-      path: '/contatos',
-      component: Contatos
-    },
-    {
-      path: '/',
-      component: Home
-    }
-  ]
-});
 
 new Vue({
   router,
